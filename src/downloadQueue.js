a456d7067709aa47397f9884d6fa3447b420b482
Clownvin <123chall@gmail.com>
2020-04-22 19:01:14 -0500

    Made it do

--- src/downloadQueue.js
+++ src/downloadQueue.js
@@ -1,83 +1,76 @@
+'use strict';
+//REQUEST IS DEPRECATED
 const request = require('request');
 const progress = require('request-progress');
 const fs = require('fs');
 
-class DownloadInstance {
-  constructor(filePath, folder, fileName) {
-    this.next = null;
-    this.previous = null;
-    this.path = filePath;
-    this.folder = folder;
-    this.name = fileName;
-    this.downloading;
-    this.download;
-    this.percent = 0;
-    this.speed = 0;
-    this.size = {
-      total: 0,
-      transferred: 0
-    };
-    this.time = {
-      elapsed: 0,
-      remaining: null
-    };
+class DownloadRequest {
+  constructor(path, folder, name) {
+    this.path = path;
+    this.folder = folder || '';
+    this.name = name;
+    this.state = {
+
+    }
+  }
+}
+
+function makeDownloadFolderIfNotExists(path) {
+  const folders = path.split(/(!\.)\//);
+  let current = '';
+  //recursive option of mkdirSync wasn't working, gave up
+  while (folders.length) {
+    current = `${current}${folders.shift()}/`;
+    if (!fs.existsSync(current)) {
+      fs.mkdirSync(current);
+    }
+  }
+}
+
+class DownloadManager {
+  constructor() {
+    this.queue = [];
+    this.currentDownload = null;
   }
 
-  startDownload(next) {
-    console.log('Download Started', this.path);
-    progress(request(this.path), {
+  addDownload(filePath, folder, name) {
+    const download = new DownloadRequest(filePath, folder, name);
+    this.queue.push(download);
+    this.startDownload();
+  }
+
+  startDownload() {
+    if (this.currentDownload) {
+      return; //Already downloading.
+    }
+    const currentDownload = this.currentDownload = this.queue.shift();
+    const downloadPath = `./downloads/${currentDownload.folder}`;
+    makeDownloadFolderIfNotExists(downloadPath);
+    progress(request(currentDownload.path), {
       // throttle: 2000,                    // Throttle the progress event to 2000ms, defaults to 1000ms
       delay: 1000,                       // Only start to emit after 1000ms delay, defaults to 0ms
       // lengthHeader: 'x-transfer-length'  // Length header to use, defaults to content-length
     })
     .on('progress', (state) => {
+      currentDownload.state = state;
       console.log('Progress', state.percent, state.speed);
+      console.log(JSON.stringify(state, null, 2));
       // this.percent = state.percent;
       // this.speed = state.speed;
       // this.size = state.size;
       // this.time = state.time;
     })
-    .on('error', function (err) {
-      console.log('Error', err);
+    .on('error', (err) => {
+      console.log(`Error occured while downloading file ${currentDownload.path}:\n`, err);
+      this.currentDownload = null;
+      this.startDownload();
     })
-    .on('end', function () {
-        // Do something after request finishes
-        console.log('end');
-        next();
+    .on('end', () => {
+      this.currentDownload = null;
+      this.startDownload();
     })
-    .pipe(fs.createWriteStream(`./downloads/${this.name}`));
-    
+    .pipe(fs.createWriteStream(`${downloadPath}/${currentDownload.name}`));
   }
 }
 
-class DownloadQueue {
-  constructor() {
-    this.front = null;
-    this.back = null;
-    this.size = 0;
-  }
-
-  addDownload(filePath, folder, fileName) {
-    let newDownload = new DownloadInstance(filePath, folder, fileName);
-    if(this.size === 0) {
-      this.front = newDownload;
-      this.back = newDownload;
-    } else {
-      let nextNode = this.back;
-      newDownload.next = this.back;
-      nextNode.previous = newDownload;
-      this.back = newDownload;
-    }
-
-    newDownload.startDownload(() => {
-      this.removeDownload();
-    });
-    this.size++;
-  }
-
-  removeDownload() {
-    console.log('Remove Download');
-  }
-}
-
-module.exports = DownloadQueue;
+module.exports = DownloadManager;
